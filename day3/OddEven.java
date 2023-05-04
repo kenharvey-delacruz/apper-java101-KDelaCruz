2897ffcf0bdc170286c8ad4fcf07e29d177316b7
kenharvey-delacruz <kenharvey.delacruz@gcash.com>
2023-05-04 22:04:52 +0800

    Main - Update OddEven.java with comments

--- day3/OddEven.java
+++ day3/OddEven.java
@@ -1,5 +1,11 @@
 public class OddEven {
 
+// Java program that determines whether a number is odd or even, 
+// and then returns the total number of odd and even numbers.
+// array nums contains the numbers that will be checked
+// array ans contains the number of odd and even numbers
+// ans[0] contains # of odd, ans[1] contains # of even
+
     public static void main(String[] args) {
         int[] nums = new int[]{1, 234, 2, 4, 56, 17, 50, 1235, 6, 1, 23, 48, 47, 34};
         int[] ans = CountOddEven(nums);
